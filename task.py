98b8237895fc959136f758d11c8f08bfa38a5867
sandeeppaulraj <sandeeppaulraj@gmail.com>
2018-06-09 11:21:50 -0700

    update reward function

--- task.py
+++ task.py
@@ -29,6 +29,12 @@ class Task():
     def get_reward(self):
         """Uses current pose of sim to return reward."""
         reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
+
+        if reward > 1:
+            reward = 10
+        elif reward < -1:
+            reward = -1
+
         return reward
 
     def step(self, rotor_speeds):
